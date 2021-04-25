fd7843f818c180745f6a5308975face7a71d6862
Szabolcs Szilágyi <szabolcs.szilagyi@gmx.com>
2021-04-25 21:04:10 +0200

    Add CORS

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+const origin = process.env.NODE_ENV === 'development' || /mynalabel\.com$/;
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+      origin,
+      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+      preflightContinue: false,
+      optionsSuccessStatus: 204
+  })
   await app.listen(process.env.PORT || 6000);
 }
 bootstrap();
