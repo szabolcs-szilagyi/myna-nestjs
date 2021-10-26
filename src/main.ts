4bfecc14175576637b1a14c13751cf72ea2523f1
Szabolcs Szilágyi <szabolcs.szilagyi@gmx.com>
2021-10-26 22:21:11 +0200

    feat: added basic express-session setup

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
+import * as session from 'express-session';
 import { AppModule } from './app.module';
 
+const {
+  NODE_ENV,
+  SESSION_SECRET = 'my-super-secret-dev-secret-66',
+} = process.env;
+
 const devEnv = new Set([undefined, 'dev', 'development']);
-const origin = devEnv.has(process.env.NODE_ENV) || /mynalabel\.com$/;
+const isDevelopmentEnv = devEnv.has(NODE_ENV);
+const origin = isDevelopmentEnv || /mynalabel\.com$/;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -12,6 +19,20 @@ async function bootstrap() {
     preflightContinue: false,
     optionsSuccessStatus: 204,
   });
+  app.use(
+    session({
+      secret: SESSION_SECRET,
+      resave: false,
+      saveUninitialized: true,
+      name: `${NODE_ENV || 'development'}-myna`,
+      cookie: {
+        domain: '*',
+        maxAge: 24 * 60 * 60 * 1000,
+        httpOnly: isDevelopmentEnv,
+        secure: !isDevelopmentEnv,
+      },
+    }),
+  );
   await app.listen(process.env.PORT || 7000);
 }
 bootstrap();
