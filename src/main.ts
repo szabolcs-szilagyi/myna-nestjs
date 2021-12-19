486e2a69a48e855148bd014b1f498ae9d0a1e2bd
Szabolcs Szilágyi <szabolcs.szilagyi@gmx.com>
2021-12-19 18:08:31 +0100

    fix: corrected cors settings so to get it working on local

--- src/main.ts
+++ src/main.ts
@@ -5,12 +5,13 @@ const { NODE_ENV } = process.env;
 
 const devEnv = new Set([undefined, 'dev', 'development', '127.0.0.1']);
 const isDevelopmentEnv = devEnv.has(NODE_ENV);
-const origin = isDevelopmentEnv || /mynalabel\.com$/;
+const origin = isDevelopmentEnv ? /localhost/ : /mynalabel\.com$/;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
     origin,
+    credentials: true,
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     preflightContinue: false,
     optionsSuccessStatus: 204,
