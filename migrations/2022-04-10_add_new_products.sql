890157156ceecd9ec119a7fa3d957f0eaff51eb1
Szabolcs Szilágyi <szabolcs.szilagyi@gmx.com>
2022-04-10 15:05:43 +0200

    chore: added new products

---
+++ migrations/2022-04-10_add_new_products.sql
@@ -0,0 +1,172 @@
+INSERT INTO public.products (idname, availability, is_one_size, productname, productcolor, productprice, desclong, comp_care, pic1, pic2, pic3, pic4, pic5, pic6, pic7, pic8, pic9, name_pl,  desc_pl,  composition_and_care_pl) VALUES
+   (
+/* idname */          'leya-dress'
+/* availability */  , 'Available'
+/* is_one_size */   , false
+/* productname */   , 'LEYA Dress'
+/* productcolor */  , 'Dusty Rose'
+/* productprice */  , 360
+/* desclong */      , '<div class="ju">100% organic cotton flowy maxi dress with extra lining. Feminine and elegant LEYA dress it’s quite versatile, for any occasion – elegant or casual – depend on your style or mood.  LEYA dress is lightweight and surprises with crafted embroidery at the upper front and upper back of the dress. Sustainable buttons from corozo nuts perfectly unite the whole look and style.<br />Sustainably made with love in Europe by local tailors. Look at the composition and care of this amazing quality.</div>'
+/* comp_care */     , '<div class="ju">100% organic cotton voile, GOTS certified, fabric from Turkey.<br />Free from microplastics, 100 % biodegradable.<br />Wash on delicate cycle at 30 Celsius degrees, iron at low setting and do not tumble dry.<br />Our fabrics are sustainable and do not require dry cleaning.</div>'
+/* pic1 */          , 'leya-dress-1.jpg'
+/* pic2 */          , 'leya-dress-2.jpg'
+/* pic3 */          , 'leya-dress-3.jpg'
+/* pic4 */          , 'leya-dress-4.jpg'
+/* pic5 */          , NULL
+/* pic6 */          , NULL
+/* pic7 */          , NULL
+/* pic8 */          , NULL
+/* pic9 */          , NULL
+/* name_pl */       , 'LEYA Dress'
+/* desc_pl */       , '<div class="ju">100% organic cotton flowy maxi dress with extra lining. Feminine and elegant LEYA dress it’s quite versatile, for any occasion – elegant or casual – depend on your style or mood.  LEYA dress is lightweight and surprises with crafted embroidery at the upper front and upper back of the dress. Sustainable buttons from corozo nuts perfectly unite the whole look and style.<br />Sustainably made with love in Europe by local tailors. Look at the composition and care of this amazing quality.</div>'
+/* comp_care_pl */  , '<div class="ju">100% organic cotton voile, GOTS certified, fabric from Turkey.<br />Free from microplastics, 100 % biodegradable.<br />Wash on delicate cycle at 30 Celsius degrees, iron at low setting and do not tumble dry.<br />Our fabrics are sustainable and do not require dry cleaning.</div>'
+   )
+   , (
+/* idname */          'rosa-dress'
+/* availability */  , 'Available'
+/* is_one_size */   , false
+/* productname */   , 'ROSA Dress'
+/* productcolor */  , 'Dusty Rose'
+/* productprice */  , 260
+/* desclong */      , '<div class="ju">100% organic cotton, short and extra flowy dress. ROSA is very light thanks to voile quality. Beautiful embroidery included on the top of the dress came from our inspiration which you can see across the whole collection. Long opening at the back of ROSA dress creates surprising detail together with buttons from corozo nuts. Enjoy our feminine and loved design.<br />Sustainably made with love in Europe by local tailors. Look at the composition and care of this amazing quality.</div>'
+/* comp_care */     , '<div class="ju">100% organic cotton voile, GOTS certified, fabric from Turkey.<br />Free from microplastics, 100 % biodegradable.<br />Wash on delicate cycle at 30 celsius degrees, iron at low setting and do not tumble dry.<br />Our fabrics are sustainable and do not require dry cleaning.</div>'
+/* pic1 */          , 'rosa-dress-1.jpg'
+/* pic2 */          , 'rosa-dress-2.jpg'
+/* pic3 */          , 'rosa-dress-3.jpg'
+/* pic4 */          , 'rosa-dress-4.jpg'
+/* pic5 */          , NULL
+/* pic6 */          , NULL
+/* pic7 */          , NULL
+/* pic8 */          , NULL
+/* pic9 */          , NULL
+/* name_pl */       , 'ROSA Dress'
+/* desc_pl */       , '<div class="ju">100% organic cotton, short and extra flowy dress. ROSA is very light thanks to voile quality. Beautiful embroidery included on the top of the dress came from our inspiration which you can see across the whole collection. Long opening at the back of ROSA dress creates surprising detail together with buttons from corozo nuts. Enjoy our feminine and loved design.<br />Sustainably made with love in Europe by local tailors. Look at the composition and care of this amazing quality.</div>'
+/* comp_care_pl */  , '<div class="ju">100% organic cotton voile, GOTS certified, fabric from Turkey.<br />Free from microplastics, 100 % biodegradable.<br />Wash on delicate cycle at 30 celsius degrees, iron at low setting and do not tumble dry.<br />Our fabrics are sustainable and do not require dry cleaning.</div>'
+   )
+   , (
+/* idname */          'zinia-skirt'
+/* availability */  , 'Available'
+/* is_one_size */   , false
+/* productname */   , 'ZINIA Skirt'
+/* productcolor */  , 'Dusty Rose'
+/* productprice */  , 190
+/* desclong */      , '<div class="ju">100% organic cotton maxi skirt. ZINIA surprises with unique pattern and flexibility. Comfortable and feminine design will be your loved style and timeless addition to your capsule wardrobe.<br />Sustainably made with love in Europe by local tailors. Look at the composition and care of this amazing quality.</div>'
+/* comp_care */     , '<div class="ju">100% organic cotton voile, GOTS certified, fabric from Turkey.<br />Free from microplastics, 100 % biodegradable.<br />Wash on delicate cycle at 30 celsius degrees, iron at low setting and do not tumble dry.<br />Our fabrics are sustainable and do not require dry cleaning.</div>'
+/* pic1 */          , 'zinia-skirt-1.jpg'
+/* pic2 */          , 'zinia-skirt-2.jpg'
+/* pic3 */          , 'zinia-skirt-3.jpg'
+/* pic4 */          , 'zinia-skirt-4.jpg'
+/* pic5 */          , NULL
+/* pic6 */          , NULL
+/* pic7 */          , NULL
+/* pic8 */          , NULL
+/* pic9 */          , NULL
+/* name_pl */       , 'ZINIA Skirt'
+/* desc_pl */       , '<div class="ju">100% organic cotton maxi skirt. ZINIA surprises with unique pattern and flexibility. Comfortable and feminine design will be your loved style and timeless addition to your capsule wardrobe.<br />Sustainably made with love in Europe by local tailors. Look at the composition and care of this amazing quality.</div>'
+/* comp_care_pl */  , '<div class="ju">100% organic cotton voile, GOTS certified, fabric from Turkey.<br />Free from microplastics, 100 % biodegradable.<br />Wash on delicate cycle at 30 celsius degrees, iron at low setting and do not tumble dry.<br />Our fabrics are sustainable and do not require dry cleaning.</div>'
+   )
+   , (
+/* idname */          'aster-frill'
+/* availability */  , 'Available'
+/* is_one_size */   , false
+/* productname */   , 'ASTER Frill'
+/* productcolor */  , 'Midnight Rose'
+/* productprice */  , 160
+/* desclong */      , '<div class="ju">100% organic cotton comfortable fit culottes for easy look everyday. Our feminine design of ASTER FRILL surprises with unique details and flexibility. Extremely soft fabric creates that extra comfort, that you need to feel amazing through out the whole day.<br />Sustainably made with love in Europe by local tailors. Look at the composition and care of this amazing quality.</div>'
+/* comp_care */     , '<div class="ju">GOTS certified natural fiber fabric. A soft interlock jersey made of 100% organic cotton, slightly stretchy and very soft on the skin.<br />Free from microplastics, 100 % biodegradable.<br />Wash on delicate cycle at 30 Celsius degrees, iron at low setting and do not tumble dry.<br />Our fabrics are sustainable and do not require dry cleaning.</div>'
+/* pic1 */          , 'aster-frill-1.jpg'
+/* pic2 */          , 'aster-frill-2.jpg'
+/* pic3 */          , 'aster-frill-3.jpg'
+/* pic4 */          , 'aster-frill-4.jpg'
+/* pic5 */          , NULL
+/* pic6 */          , NULL
+/* pic7 */          , NULL
+/* pic8 */          , NULL
+/* pic9 */          , NULL
+/* productname */   , 'ASTER Frill'
+/* desclong */      , '<div class="ju">100% organic cotton comfortable fit culottes for easy look everyday. Our feminine design of ASTER FRILL surprises with unique details and flexibility. Extremely soft fabric creates that extra comfort, that you need to feel amazing through out the whole day.<br />Sustainably made with love in Europe by local tailors. Look at the composition and care of this amazing quality.</div>'
+/* comp_care */     , '<div class="ju">GOTS certified natural fiber fabric. A soft interlock jersey made of 100% organic cotton, slightly stretchy and very soft on the skin.<br />Free from microplastics, 100 % biodegradable.<br />Wash on delicate cycle at 30 Celsius degrees, iron at low setting and do not tumble dry.<br />Our fabrics are sustainable and do not require dry cleaning.</div>'
+   )
+   , (
+/* idname */          'erica-sweatshirt'
+/* availability */  , 'Available'
+/* is_one_size */   , false
+/* productname */   , 'ERICA Sweatshirt'
+/* productcolor */  , 'Midnight Rose'
+/* productprice */  , 150
+/* desclong */      , '<div class="ju">100% organic cotton comfortable and casual sweatshirt for everyday look. Cropped style and long pocket on one side creates stylish approach to our sweatshirt. Wear ERICA with with our ASTER FRILL for a perfect match or separately for a versatile look.<br />Sustainably made with love in Europe by local tailors. Look at the composition and care of this amazing quality.</div>'
+/* comp_care */     , '<div class="ju">GOTS certified natural fiber fabric. A soft interlock jersey made of 100% organic cotton, slightly stretchy and very soft on the skin. Free from microplastics, 100 % biodegradable.<br />Wash on delicate cycle at 30 celsius degrees, iron at low setting and do not tumble dry.<br />Our fabrics are sustainable and do not require dry cleaning.</div>'
+/* pic1 */          , 'erica-sweatshirt-1.jpg'
+/* pic2 */          , 'erica-sweatshirt-2.jpg'
+/* pic3 */          , 'erica-sweatshirt-3.jpg'
+/* pic4 */          , 'erica-sweatshirt-4.jpg'
+/* pic5 */          , NULL
+/* pic6 */          , NULL
+/* pic7 */          , NULL
+/* pic8 */          , NULL
+/* pic9 */          , NULL
+/* productname */   , 'ERICA Sweatshirt'
+/* desclong */      , '<div class="ju">100% organic cotton comfortable and casual sweatshirt for everyday look. Cropped style and long pocket on one side creates stylish approach to our sweatshirt. Wear ERICA with with our ASTER FRILL for a perfect match or separately for a versatile look.<br />Sustainably made with love in Europe by local tailors. Look at the composition and care of this amazing quality.</div>'
+/* comp_care */     , '<div class="ju">GOTS certified natural fiber fabric. A soft interlock jersey made of 100% organic cotton, slightly stretchy and very soft on the skin. Free from microplastics, 100 % biodegradable.<br />Wash on delicate cycle at 30 celsius degrees, iron at low setting and do not tumble dry.<br />Our fabrics are sustainable and do not require dry cleaning.</div>'
+   )
+   , (
+/* idname */          'hana-oversize-shirt'
+/* availability */  , 'Available'
+/* is_one_size */   , false
+/* productname */   , 'HANA Oversize Shirt'
+/* productcolor */  , 'Satin Cream'
+/* productprice */  , 165
+/* desclong */      , '<div class="ju">100% linen quality shirt. Classic design of our HANA shirt represents timeless look. This shirt is cut for an oversize fit for your comfort  and loved style for years to come.  Additionally HANA has one pocket at the front.<br />Sustainably made with love in Europe by local tailors. Look at the composition and care of this amazing quality.</div>'
+/* comp_care */     , '<div class="ju">100% linen, fabric from Turkey.<br />Wash on delicate cycle at 30 Celsius degrees, iron at low setting and do not tumble dry.<br />Our fabrics are sustainable and do not require dry cleaning.</div>'
+/* pic1 */          , 'hana-oversize-shirt-1.jpg'
+/* pic2 */          , 'hana-oversize-shirt-2.jpg'
+/* pic3 */          , 'hana-oversize-shirt-3.jpg'
+/* pic4 */          , 'hana-oversize-shirt-4.jpg'
+/* pic5 */          , NULL
+/* pic6 */          , NULL
+/* pic7 */          , NULL
+/* pic8 */          , NULL
+/* pic9 */          , NULL
+/* productname */   , 'HANA Oversize Shirt'
+/* desclong */      , '<div class="ju">100% linen quality shirt. Classic design of our HANA shirt represents timeless look. This shirt is cut for an oversize fit for your comfort  and loved style for years to come.  Additionally HANA has one pocket at the front.<br />Sustainably made with love in Europe by local tailors. Look at the composition and care of this amazing quality.</div>'
+/* comp_care */     , '<div class="ju">100% linen, fabric from Turkey.<br />Wash on delicate cycle at 30 Celsius degrees, iron at low setting and do not tumble dry.<br />Our fabrics are sustainable and do not require dry cleaning.</div>'
+   )
+   ON CONFLICT (idname) DO UPDATE SET
+   availability            = EXCLUDED.availability,
+   is_one_size             = EXCLUDED.is_one_size,
+   productname             = EXCLUDED.productname,
+   productcolor            = EXCLUDED.productcolor,
+   productprice            = EXCLUDED.productprice,
+   desclong                = EXCLUDED.desclong,
+   comp_care               = EXCLUDED.comp_care,
+   pic1                    = EXCLUDED.pic1,
+   pic2                    = EXCLUDED.pic2,
+   pic3                    = EXCLUDED.pic3,
+   pic4                    = EXCLUDED.pic4,
+   pic5                    = EXCLUDED.pic5,
+   pic6                    = EXCLUDED.pic6,
+   pic7                    = EXCLUDED.pic7,
+   pic8                    = EXCLUDED.pic8,
+   pic9                    = EXCLUDED.pic9,
+   name_pl                 = EXCLUDED.name_pl,
+   desc_pl                 = EXCLUDED.desc_pl,
+   composition_and_care_pl = EXCLUDED.composition_and_care_pl
+   ;
+
+
+INSERT INTO public.stock
+  (idname,                 xs, s, m, ml, l, one_size) VALUES
+  ('leya-dress',           5,  5, 5, 5,  5, NULL)
+, ('rosa-dress',           5,  5, 5, 5,  5, NULL)
+, ('zinia-skirt',          5,  5, 5, 5,  5, NULL)
+, ('aster-frill',          5,  5, 5, 5,  5, NULL)
+, ('erica-sweatshirt',     5,  5, 5, 5,  5, NULL)
+, ('hana-oversize-shirt',  5,  5, 5, 5,  5, NULL)
+ ON CONFLICT (idname) DO UPDATE SET
+ xs       = EXCLUDED.xs,
+ s        = EXCLUDED.s,
+ m        = EXCLUDED.m,
+ ml       = EXCLUDED.ml,
+ l        = EXCLUDED.l,
+ one_size = EXCLUDED.one_size
+ ;
