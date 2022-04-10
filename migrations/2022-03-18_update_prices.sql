0e486f0f0980f0e0442115fe8eb18c76afeeb8d1
Szabolcs Szilágyi <szabolcs.szilagyi@gmx.com>
2022-04-10 11:06:10 +0200

    chore: updated some product prices

---
+++ migrations/2022-03-18_update_prices.sql
@@ -0,0 +1,6 @@
+UPDATE public.products SET productprice = 170 WHERE idname = 'lola-oversized-shirt';
+UPDATE public.products SET productprice = 115 WHERE idname = 'delphi-culottes';
+UPDATE public.products SET productprice = 150 WHERE idname = 'tilia-blouse';
+UPDATE public.products SET productprice = 270 WHERE idname = 'marigold-trench-coat';
+UPDATE public.products SET productprice = 120 WHERE idname = 'calla-cream';
+UPDATE public.products SET productprice = 360 WHERE idname = 'leya-wrap-dress';
