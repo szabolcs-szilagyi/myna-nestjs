b63e40cc68607c93ed5b03ffc5d616914227cd79
Szabolcs Szilágyi <szabolcs.szilagyi@gmx.com>
2022-08-28 16:09:38 +0200

    chore(product): updated prices

--- src/ormconfig.ts
+++ src/ormconfig.ts
@@ -35,7 +35,7 @@ const config: ConnectionOptions = {
   // Run migrations automatically,
   // you can disable this if you prefer running migration manually.
   migrationsRun: false,
-  logging: true,
+  logging: ['error', 'warn', 'info'],
   //logger: 'file',
 
   // allow both start:prod and start:dev to use migrations
